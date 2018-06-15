42e89e2a7b0ee0b11fc4ee361f6849d4c836162f
法本-张少楠 <farben23@boshi.com.cn>
2018-06-15 09:41:53 +0800

    ''

--- color_openpyxl.py
+++ color_openpyxl.py
@@ -1,22 +1,20 @@
+'''color_openpyxl颜色序号生成列表'''
 from openpyxl.workbook import Workbook
 from openpyxl.styles import colors
 from openpyxl.styles import PatternFill
 
+#新建一个excel文件
 wb = Workbook()
+#使用激活中的sheet，就是sheet1
 ws = wb.active
 
-ws.print_options.horizontalCentered = True
-ws.print_options.verticalCentered = True
 
-
-
+#颜色参数元组
 color = colors.COLOR_INDEX
-print(color)
 for i in range(64):
     v = color[i]
-    print(v)
     ws['A%d'%(i+1)] = v
     ws['A%d'%(i+1)].fill = PatternFill(fill_type='solid', fgColor=v)
 
 
-wb.save('color_openpyxl1.xlsx')
+wb.save('color_openpyxl.xlsx')
